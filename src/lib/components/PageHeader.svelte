bee7146ea182cb83c053cc84a7dec5201c052263
Thomas Lamars <thomas.lamars@wearetriple.com>
2024-08-21 22:39:50 +0200

    feat(work): adjusted page header and work-info

--- src/lib/components/PageHeader.svelte
+++ src/lib/components/PageHeader.svelte
@@ -1,11 +1,20 @@
 <script lang="ts">
   export let title: string;
-  export let subtitle: string | undefined;
+  export let subtitle: string | undefined = undefined;
+  export let marginSize: "title-page" | "sub-page" = "title-page";
+
+  console.log(marginSize);
 </script>
 
-<header class="header">
+<header
+  class="header"
+  class:title-page={marginSize === "title-page"}
+  class:sub-page={marginSize === "sub-page"}
+>
   <h1 class="title">{@html title}</h1>
-  <h3 class="subtitle">{@html subtitle}</h3>
+  {#if subtitle}
+    <h3 class="subtitle">{@html subtitle}</h3>
+  {/if}
 </header>
 
 <style lang="scss">
@@ -17,8 +26,26 @@
     align-items: center;
     justify-content: center;
     text-align: center;
-    margin-top: 28rem;
-    margin-bottom: 28rem;
+
+    &.title-page {
+      margin-top: 28rem;
+      margin-bottom: 28rem;
+
+      @include phone-only {
+        margin-top: 20rem;
+        margin-bottom: 20rem;
+      }
+    }
+
+    &.sub-page {
+      margin-top: 19.2rem;
+      margin-bottom: 9.2rem;
+
+      @include phone-only {
+        margin-top: 12rem;
+        margin-bottom: 8em;
+      }
+    }
   }
 
   .title {
